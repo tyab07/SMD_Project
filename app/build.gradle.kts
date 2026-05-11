6ec3235264998f939d81dfbb9858bab8ffecda2a
Og-Brutal <ogbrutal2825@gmail.com>
2026-05-11 12:09:58 +0500

    Fix formatting in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.fastconnect"
+        applicationId = "com.example.fastconnect" 
         minSdk = 26
         targetSdk = 35
         versionCode = 1
