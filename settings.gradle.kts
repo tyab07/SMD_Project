ad17fa37415faa953a69045a1bbe9803d94f2b35
Og-Brutal <ogbrutal2825@gmail.com>
2026-05-12 19:48:31 +0500

    Fix includeGroupByRegex for androidx in settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
             content {
                 includeGroupByRegex("com\\.android.*")
                 includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
+                includeGroupByRegex("androidx.*") 
             }
         }
         mavenCentral()
